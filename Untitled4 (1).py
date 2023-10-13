ff17e391517c66aa899d86e2c5fc239105b828f3
thasni16 <145977690+thasni16@users.noreply.github.com>
2023-10-13 11:58:01 +0530

    Add files via upload

---
+++ Untitled4 (1).py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+def gcd(a,b):
+    if b==0:
+        return a
+    else:
+        return gcd(b,a%b)
+n1=int(input("Enter first number:"))
+n2=int(input("Enter second number:"))
+g=gcd(n1,n2)
+print("gcd(num1,num2):",g)
+
+
+# In[ ]:
+
+
+def hello():
+    print("Hello world")
+hello()
+
+
+# In[15]:
+
+
+def sum():
+    print("sum is",a+b)
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+sum()
+    
+    
+
+
+# In[22]:
+
+
+list=[2,5,6,7,8,9]
+print(list)
+for i in list:
+    if i%2==0:
+        list.remove(i)
+print("Display the numbers after removing even numbers")
+print(list)
+
+
+# In[ ]:
+
+
+
+
