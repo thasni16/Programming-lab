29947b657b8846207add60811497c8890bdc868f
thasni16 <145977690+thasni16@users.noreply.github.com>
2023-12-02 10:51:56 +0530

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = lambda a : a*a
+print(x(5))
+x = lambda a,b : a * b
+print(x(5, 6))
+x = lambda  b,h : 0.5*b*h
+print(x(5, 6))
+
+
+# In[ ]:
+
+
+
+
