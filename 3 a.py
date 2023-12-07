9a0dabd9654d25b41c8db12711ccee542daf9563
thasni16 <145977690+thasni16@users.noreply.github.com>
2023-12-07 11:05:05 +0530

    Add files via upload

---
+++ 3 a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+list=[]
+for i in range(0,3):
+    n=int(input("Enter integer:"))
+    list.append(n)
+print(list)
+list=[i for i in list if i>0]
+print("positive numbers of the list",list)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
