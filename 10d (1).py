f347396fafb9a32800ef35d5aa40c67d230c5f94
thasni16 <145977690+thasni16@users.noreply.github.com>
2023-12-08 15:06:10 +0530

    Add files via upload

---
+++ 10d (1).py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[45]:
+
+
+import csv
+with open('dep.csv','r') as file:
+    data=csv.reader(file)
+    for i in data:
+        print(i[0],i[1])
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
